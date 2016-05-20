20da61818e6bed5e80f1176ccc8b914ce98a6fa2
Max Beier <beierm@htw-berlin.de>
2016-05-20 15:30:51 +0200

    Items aktualisiert und Styles dazu angepasst

--- assets/js/config.js
+++ assets/js/config.js
@@ -9,26 +9,78 @@ window.config = {
    },
 
    marker: [{
-      left: '15.1%',
-      top: '19.7%',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\
-                <button class="item" type="button" data-price="15">Fahne</button>',
+      left: '60%',
+      top: '64.5%',
+      content: '<button class="item" type="button" data-price="2800">Motor (Austausch)</button>\
+                <button class="item" type="button" data-price="1500">Getriebe (Austausch)</button>\
+                <button class="item" type="button" data-price="450">Nockenwelle</button>\
+                <button class="item" type="button" data-price="300">Ansaugkrümmer</button>\
+                <button class="item" type="button" data-price="500">Ölwanne </button>\
+                <button class="item" type="button" data-price="39">Ölwannendichtung</button>\
+                <button class="item" type="button" data-price="1100">Einspritzpumpe</button>\
+                <button class="item" type="button" data-price="450">Hydraulikpumpe</button>\
+                <button class="item" type="button" data-price="350">Hydaulikpumpenantrieb</button>\
+                <button class="item" type="button" data-price="29">Kupplungsnehmerzylinder</button>\
+                <button class="item" type="button" data-price="250">Kupplungsscheibe</button>\
+                <button class="item" type="button" data-price="250">Kupplungsdruckplatte</button>\
+                <button class="item" type="button" data-price="700">Lenkgetriebe</button>\
+                <button class="item" type="button" data-price="590">Lenkschubstange</button>\
+                <button class="item" type="button" data-price="220">Spurstange</button>\
+                <button class="item" type="button" data-price="15">Ölfilter</button>\
+                <button class="item" type="button" data-price="110">Luftfilter</button>\
+                <button class="item" type="button" data-price="39">Kraftstoffilter</button>',
    }, {
-      left: '20.8%',
-      top: '56.7%',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\
-                <button class="item" type="button" data-price="150">Reifen</button>\
-                <button class="item" type="button" data-price="50">Felge</button>',
+      left: '72.6%',
+      top: '69.7%',
+      content: '<button class="item" type="button" data-price="390">Auspuffkrümmer</button>\
+                <button class="item" type="button" data-price="90">Schalldämpfer</button>\
+                <button class="item" type="button" data-price="140">Batterie</button>\
+                <button class="item" type="button" data-price="290">Anlasser</button>\
+                <button class="item" type="button" data-price="220">Lichtmaschine</button>\
+                <button class="item" type="button" data-price="30">Lichtmaschinenregler</button>',
    }, {
-      left: '54%',
-      top: '59.9%',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\
-                <button class="item" type="button" data-price="75">Benzinpumpe</button>',
+      left: '11.1%',
+      top: '66.7%',
+      content: '<button class="item" type="button" data-price="950">Achse vorn</button>\
+                <button class="item" type="button" data-price="750">Tragring für Allrad Vorderachse</button>\
+                <button class="item" type="button" data-price="450">Achse hinten</button>\
+                <button class="item" type="button" data-price="350">Räder</button>\
+                <button class="item" type="button" data-price="55">Radbremszylinder</button>\
+                <button class="item" type="button" data-price="240">Bremstrommel</button>\
+                <button class="item" type="button" data-price="500">Bremskraftverstärker</button>\
+                <button class="item" type="button" data-price="65">Druckrelais Bremsventil</button>\
+                <button class="item" type="button" data-price="110">Bremsbeläge </button>\
+                <button class="item" type="button" data-price="300">Bremsleitungen Stahlflex (Satz)</button>\
+                <button class="item" type="button" data-price="190">Anhängerbremsventil</button>',
    }, {
-      left: '54%',
-      top: '30%',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\
-                <button class="item" type="button" data-price="75">Wand</button>',
+      left: '14%',
+      top: '46%',
+      content: '<button class="item" type="button" data-price="200">Tür </button>\
+                <button class="item" type="button" data-price="62">Türdichtungen</button>\
+                <button class="item" type="button" data-price="47">Türgriff mit Schließzylinder</button>\
+                <button class="item" type="button" data-price="30">Türgriff ohne Schließzylinder</button>\
+                <button class="item" type="button" data-price="135">Fensterheber</button>\
+                <button class="item" type="button" data-price="20">Fensterkurbel</button>\
+                <button class="item" type="button" data-price="25">Ausstellmechanik Drehfenster</button>\
+                <button class="item" type="button" data-price="69">Sicherheitsgurt</button>\
+                <button class="item" type="button" data-price="150">Scheibenwischermotor</button>\
+                <button class="item" type="button" data-price="25">Scheibenwischergestänge</button>\
+                <button class="item" type="button" data-price="12">Scheibenwischerarm</button>\
+                <button class="item" type="button" data-price="30">Scheibenwischerblatt</button>\
+                <button class="item" type="button" data-price="25">Sonnenblende</button>\
+                <button class="item" type="button" data-price="24">Spiegel</button>',
+   }, {
+      left: '8%',
+      top: '57%',
+      content: '<button class="item" type="button" data-price="110">Scheinwerfer </button>\
+                <button class="item" type="button" data-price="50">Nebelscheinwerfer</button>\
+                <button class="item" type="button" data-price="35">Rücklicht</button>\
+                <button class="item" type="button" data-price="15">Blinker vorn</button>\
+                <button class="item" type="button" data-price="15">Blinker hinten</button>\
+                <button class="item" type="button" data-price="10">Kennzeichenbeleuchtung</button>\
+                <button class="item" type="button" data-price="80">Signalhorn</button>\
+                <button class="item" type="button" data-price="250">Stoßstange vorn</button>\
+                <button class="item" type="button" data-price="120">Stoßstange hinten</button>',
    }],
 
 };
